5b10211897d72b5da165bf7f77911cbd326ba416
JKehoe97 <jkkehoe177@gmail.com>
2018-05-26 17:42:35 +1000

    added manifest back to Classes folder

---
+++ A2/src/sMart/Classes/Manifest.java
@@ -0,0 +1,91 @@
+package sMart;
+
+/**
+ * Manifest is a collection of trucks
+ * 
+ * @author James Kehoe
+ *
+ */
+
+public class Manifest {
+	
+	private Object[][] manifest;
+	
+	public Manifest (Object [][] manifest) {
+		this.manifest = manifest;
+	}
+	
+	public double AssignTrucks() {
+		//total is 
+		double totalcost = 0;
+		int r_truck_needed = 0;
+		int o_truck_needed = 0;
+		
+		return totalcost;
+		
+		/*for (int o = 0; o < manifest.length; o++) {
+			if (manifest[o][6] != null) {
+				if (CheckFull(r_truck) == true) {
+					AddtoTruck(truck, cargo);
+				} else {
+					NewTruck(r_truck);
+				}
+			} else {
+				boolean need_fridge = false;
+				for (int r = 0; r < manifest.length; r++) {
+					if (manifest[r][6] == temp_controlled) {
+						need_fridge = true;
+					} else {
+						return;
+					}
+				}
+				if (need_fridge == true) {
+					if (CheckFull(r_truck) == true) {
+						AddtoTruck(truck, cargo);
+					} else {
+						NewTruck(r_truck);
+					}
+				} else {
+					if (CheckFull(o_truck) == true) {
+						AddtoTruck(truck, cargo);
+					} else {
+						NewTruck(o_truck);
+					}
+				}
+			}
+		}*/
+		/*
+		For item in report {
+			if temp_controlled {
+				if open space in current r_truck {
+					item in current r_truck
+				} else {
+					item in new r_truck
+				}
+			} else {
+				if open space in o_truck {
+					item in o_truck
+				} else {
+					bool need_fridge = false;
+					for (r = report[i]; r < report.length; r++) {
+						if r == temp_controlled {
+							need_fridge = true
+						} else {
+							return
+						}
+					}
+					if need_fridge == true {
+						item in new r_truck
+					} else {
+						item in new o_truck
+					}
+				}
+			}
+		}
+		*/
+	}
+	
+	public void ExportManifest() {
+		
+	}
+}
