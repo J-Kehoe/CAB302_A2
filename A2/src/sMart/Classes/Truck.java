86b176b3f8600614baf5143493a8ee553df42705
Lara <laradem96@gmail.com>
2018-05-27 16:20:41 +1000

    Truck abstract class and subclasses
    Truck, ordinary truck and refridgerated truck are complete.

--- A2/src/sMart/Classes/Truck.java
+++ A2/src/sMart/Classes/Truck.java
@@ -5,7 +5,7 @@ import java.util.*;
 /**
  * An abstract class for ordinary truck and refrigerated truck. It contains the properties cost, cargo capacity and cargo.
  * 
- * @author 
+ * @author Lara de Maroussem
  *
  */
 
@@ -13,18 +13,16 @@ public abstract class Truck {
 	
 	double cost;
 	double cargoCapacity;
-	boolean tempControlled;
-	private Item[] data;
-	private Stock stock = new Stock (data);
-	
-	private RefrigeratedTruck test = new RefrigeratedTruck();
-	private OrdinaryTruck test2 = new OrdinaryTruck();
 
 /*---------------------------------------------------------------*/	
 	
 	/**
-	   * 
-	   * @param 
+	   * This method returns the calculated cost of truck. The
+	   * full functionality of this class is implemented in both
+	   * Refrigerated and Ordinary truck.
+	   * @param double variable. This represents two seperate 
+	   * values each truck subclass needs to call.
+	   * @return cost.
 	   */
 	
 	public double calculateCost(double variable) {
@@ -34,23 +32,37 @@ public abstract class Truck {
 /*---------------------------------------------------------------*/	
 	
 	/**
-	   * 
-	   * @param 
+	   * This method adds an Item to the truck cargo, cargo is 
+	   * represented in a List<Item> format. 
+	   * @param Item a, the item to be added to the cargo.
+	   * @return Cargo. Returns truck cargo.
 	   */
 	
-	public Stock cargo (Stock cargo) {
+	public List<Item> addCargo (Item a) {
+		List<Item> cargo = new ArrayList <Item> ();
+		cargo.add(a);
 		
-		this.stock = cargo;
-		
-		Item temp = data[6];
-		
-		if (tempControlled = true) {
-			
-			
-		} else if (tempControlled = false) {
-			
-		}
 		return cargo;
 	}
 
+/*---------------------------------------------------------------*/	
+	
+	/**
+	   * This method returns a boolean and checks if truck is full.
+	   * @param List<Item> cargo.
+	   * @return checkFull, boolean to check if truck is full.
+	   */
+	
+	public Boolean checkFull (List<Item> cargo) {
+		
+		Boolean checkFull = null;
+		
+		if (cargo.size() > cargoCapacity) {
+			checkFull = true;
+		} else {
+			checkFull = false;
+		}
+		return checkFull;
+	}
+	
 }
