aa21b0cc5708cba96dbd84a615f7586f60d10bd1
JKehoe97 <jkkehoe177@gmail.com>
2018-05-27 17:13:51 +1000

    Updated Ordinary Truck Test

--- A2/src/sMart/Tests/OrdinaryTruckTest.java
+++ A2/src/sMart/Tests/OrdinaryTruckTest.java
@@ -1,18 +1,125 @@
 package sMart.Tests;
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import sMart.Classes.Item;
 import sMart.Classes.OrdinaryTruck;
 
+/**
+ * Test case for the class Stock. 
+ * 
+ * @author James Kehoe
+ *
+ */
+
 public class OrdinaryTruckTest {
+
+	/* Creating variables to be used in test cases */
 	
-	OrdinaryTruck test = new OrdinaryTruck();
+	Item a = new Item ("Bread", 1, 4, 2, 300, 200, "");
+	List<Item> testList = new ArrayList<Item> ();
 	
+	OrdinaryTruck test = new OrdinaryTruck(testList);
 	
+/*---------------------------------------------------------------*/
+	
+	/**
+	   * This method is used to test the calculateCost() function in 
+	   * the OrdinaryTruck class. In the OrdinaryTruck class this 
+	   * method is of type double, with a parameter of variable and a
+	   * return of cost.
+	   */
+		
 	@Test
 	public void testCalculateCost() {
+		testList.add(a);
 		
-		assertEquals(200,test.calculateCost(3000), 10);
+		double q = testList.size();
+		double cost = 750 + (0.25 * q);
+		
+		assertEquals(cost,test.calculateCost(3000), 10);
+	}
+
+/*---------------------------------------------------------------*/
+	
+	/**
+	   * This method is used to test the addCargo() function in the  
+	   * OrdinaryTruck class. In the OrdinaryTruck class this method 
+	   * is of List<Item> type, with a parameter of an Item a and 
+	   * a return value of List<>.
+	   */
+		
+	@Test
+	public void testAddCargo() {
+		String temp = a.getTemp();
+		
+		if(a.checkTemp(temp) == false) {
+			testList.add(a);
+		}
+		
+		assertEquals(testList, test.addCargo(a));
+	}
+
+/*---------------------------------------------------------------*/
+	
+	/**
+	   * This method is used to test the checkFull() function in the  
+	   * OrdinaryTruck class. In the OrdinaryTruck class this method 
+	   * is of type boolean, with a parameter of List<item> cargo and 
+	   * a return value of a boolean. This test checks for a true 
+	   * boolean value; if the size of List<item> cargo exceeds the
+	   * cargo capacity.
+	   */
+		
+	@Test
+	public void testCheckFullYes () {
+		double cargoCap = 1000;
+		boolean check;
+		
+		while (testList.size() <= 1001) {
+			testList.add(a);
+		}
+		
+		if (testList.size() < cargoCap) {
+			check = true;
+		} else {
+			check = false;
+		}
+		
+		assertEquals(check, test.checkFull(testList));
+	}
+
+/*---------------------------------------------------------------*/
+	
+	/**
+	   * This method is used to test the checkFull() function in the  
+	   * OrdinaryTruck class. In the OrdinaryTruck class this method 
+	   * is of type boolean, with a parameter of List<item> cargo and 
+	   * a return value of a boolean. This test checks for a false
+	   * boolean value; if the size of List<item> cargo doesn't reach 
+	   * the cargo capacity.
+	   */
+		
+	@Test
+	public void testCheckFullNo () {
+		double cargoCap = 1000;
+		boolean check;
+		
+		while (testList.size() <= 999) {
+			testList.add(a);
+		}
+		
+		if (testList.size() < cargoCap) {
+			check = true;
+		} else {
+			check = false;
+		}
+		
+		assertEquals(check, test.checkFull(testList));
 	}
 
 }
